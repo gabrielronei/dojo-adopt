abf9d5c01ded89ba663a2047c260d26d3fcf958d
Gabriel Ronei <gabriheron@hotmail.com>
2023-04-04 16:25:47 -0300

    removendo import que nao era usado

--- src/main/java/br/com/alura/dojoadopt/DojoAdoptApplication.java
+++ src/main/java/br/com/alura/dojoadopt/DojoAdoptApplication.java
@@ -2,7 +2,6 @@ package br.com.alura.dojoadopt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
